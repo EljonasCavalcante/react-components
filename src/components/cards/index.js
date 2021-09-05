891e7d6bbda07b8e789893224d33b81a9020b2e4
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-09-05 17:56:39 -0300

    Feat: creating function button and cards .

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,15 +1,41 @@
+
+import { useState } from "react";
+import Button from "../button";
+
 const Card = () => {
+
+  const [valor, setValor] = useState(0)
+
+  function Adicionar () {
+    setValor(valor + 1)
+  }
+
+  function Remover () {
+    setValor(valor - 1)
+  }
+  
   return(
-    <div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+        <div className="card">
+      <div className="card-header">
+        Meu primeiro Card
+      </div>
+      <div className="card-body">
+          <Button 
+          className="btn btn-success"
+          onClick={Adicionar}>
+
+            Adicionar
+          </Button>
+          
+          <Button          
+          className="btn btn-danger"
+          onClick={Remover}>
+
+            Remover
+          </Button>      
+          <p>{valor}</p>       
+      </div>
+      </div>
   )
 
 }
